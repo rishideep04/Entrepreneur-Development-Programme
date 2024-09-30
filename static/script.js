068e171e980a97f37a15f5d37b95108917630b75
rishideep04 <139058555+rishideep04@users.noreply.github.com>
2024-10-01 01:25:08 +0530

    script.js

---
+++ static/script.js
@@ -0,0 +1,86 @@
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if(darkMode === 'enabled'){
+   enableDarkMode();
+}
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   search.classList.remove('active');
+}
+
+let search = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   search.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   sideBar.classList.toggle('active');
+   body.classList.toggle('active');
+}
+
+document.querySelector('#close-btn').onclick = () =>{
+   sideBar.classList.remove('active');
+   body.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   search.classList.remove('active');
+
+   if(window.innerWidth < 1200){
+      sideBar.classList.remove('active');
+      body.classList.remove('active');
+   }
+}
+
+function validateSignup() {
+   // Get form values
+   const name = document.getElementById("name").value;
+   const username = document.getElementById("username").value;
+   const email = document.getElementById("email").value;
+   const password = document.getElementById("password").value;
+   const reenterPassword = document.getElementById("reenterPassword").value;
+
+   // Log values for debugging
+   console.log("Name:", name);
+   console.log("Username:", username);
+   console.log("Email:", email);
+   console.log("Password:", password);
+
+   // Validate passwords
+   if (password !== reenterPassword) {
+      alert("Passwords do not match");
+      return false;
+   }
+   return true;
+}
